f2039a16f3cf7b3d857bead1c0c48baa6283aec2
cristianfchq <cristian.fchq@gmail.com>
2020-01-10 11:54:14 -0400

    algunas caracteristicas de javascript

---
+++ app.js
@@ -0,0 +1,52 @@
+//tipos de datos
+console.log("este es una cadena");
+console.log(1);
+console.log(true);
+console.log(14.3);
+console.log([1,2,3,4]);
+
+console.log({"username":"ryan","score":14.3});
+
+var nombreUsuario = "jhon";
+console.log(nombreUsuario);
+
+const PI = 3.1415;
+console.log("PI = " + PI);
+
+let name = 'Cristian'
+let lastName = 'Choque'
+let completeName = name + ' ' + lastName;
+console.log("complete name : " + completeName);
+
+let numberOne = 60;
+let numberTwo = 100;
+let result = 0;
+result = numberOne + numberTwo;
+console.log("suma = " + result);
+result = numberOne - numberTwo;
+console.log("resta = " + result);
+result = numberOne * numberTwo;
+console.log("multiplicacion = " + result);
+result = numberOne / numberTwo;
+console.log("division = " + result);
+
+//operadores
+numberOne = 100;
+numberTwo = 500;
+result = numberOne > numberTwo;
+console.log(result);
+
+numberOne = 100;
+numberTwo = 500;
+result = numberOne < numberTwo;
+console.log(result);
+
+numberOne = 100;
+numberTwo = 500;
+result = numberOne == numberTwo;
+console.log(result);
+
+numberOne = 100;
+numberTwo = 500;
+result = numberOne != numberTwo;
+console.log(result);
